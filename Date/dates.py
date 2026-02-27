f1af58e622fe9bb73e0543b7cdd7900de7b2844e
Maulen0806 <ma_nurzhanuly@kbtu.kz>
2026-02-28 00:43:27 +0600

    Update dates.py

--- Date/dates.py
+++ Date/dates.py
@@ -1,1 +1,7 @@
-
+import datetime as dt
+time1 = dt.datetime.now()
+time2 = time1.strftime("%A")
+time3 = time1.strftime("%a")
+time4 = time1.strftime("%b")
+time5 = time1.strftime("%B")
+print(time1, time2, time3, time4, time5)

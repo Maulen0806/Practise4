244fd10d61ddb65ab56ec7ac81ccf8e4b190c610
Maulen0806 <ma_nurzhanuly@kbtu.kz>
2026-02-28 00:17:19 +0600

    Update iterator.py

--- iterator/iterator.py
+++ iterator/iterator.py
@@ -1,1 +1,19 @@
+word = input()
+y = iter(word)
+print(next(y))
+print(next(y))
+for i in word:
+    print(i)
+class nums:
+    def __iter__(self):
+        self.num = 1
+        return self
+    def __next__(self):
+        n = self.num
+        self.num += 1
+        return n
+result = nums()
+res_iter = iter(result)
 
+for i in range(10):
+    print(next(res_iter))

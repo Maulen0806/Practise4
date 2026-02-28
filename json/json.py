c39088962bea2cbf6f7cb407f86169df9db26188
Maulen0806 <ma_nurzhanuly@kbtu.kz>
2026-02-28 07:26:15 +0600

    Update json.py

--- json/json.py
+++ json/json.py
@@ -1,1 +1,6 @@
-
+import json as js
+js_dict = {"name" : "John", "age": 38, "is_student" : False, "hobbies" : ["reading", "playing games", "construction"]}
+json_string = js.dumps(js_dict)    # преобразуем в JSON-строку
+print(json_string)                       
+data = js.loads(json_string)        # обратно в Python-объект       
+print(data["name"])     
